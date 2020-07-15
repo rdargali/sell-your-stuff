8b79469f9940990fba39ea4be6aca8517e98765a
Rawand Dargali <rdargali@yahoo.com>
2020-07-15 18:01:31 -0500

    adding products and info to database

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,35 @@ const express = require("express");
 const router = express.Router();
 const formidable = require("formidable");
 const { v4: uuidv4 } = require("uuid");
+const models = require("../models");
 
 let uniqueFilename = " ";
 
 router.get("/add-product", (req, res) => {
   res.render("users/add-product");
+});
+
+router.post("/add-product", async (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let price = parseFloat(req.body.price);
+  let userId = req.session.user.userId;
+
+  let product = models.Product.build({
+    title: title,
+    description: description,
+    price: price,
+    userId: userId,
+    imageURL: uniqueFilename,
+  });
+
+  let persistedProduct = await product.save();
+
+  if (persistedProduct != null) {
+    res.redirect("/user/products");
+  } else {
+    res.render("users/add-product", { message: "Unable to add product" });
+  }
 });
 
 function uploadFile(req, callback) {
@@ -25,9 +49,9 @@ function uploadFile(req, callback) {
 
 router.post("/upload", (req, res) => {
   uploadFile(req, (photoURL) => {
-    newPhotoUrl = `/uploads/${photoURL}`;
+    photoURL = `/uploads/${photoURL}`;
     res.render("users/add-product", {
-      imageURL: newPhotoUrl,
+      imageURL: photoURL,
       className: "product-preview-image",
     });
   });
