d0e5f1468d7663e822f16f627e921199fb4553ce
Rawand Dargali <rdargali@yahoo.com>
2020-07-16 17:40:27 -0500

    added foreign key to comments table column postid

---
+++ migrations/20200716223527-adding-fk-constraint-on-productid-in-comments-table.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addConstraint("Comments", {
+      fields: ["productId"],
+      type: "FOREIGN KEY",
+      name: "postid-fk-in-comments",
+      references: {
+        table: "Products",
+        field: "id",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeConstraint("Comments", "postid-fk-in-comments");
+  },
+};
