40711e4c5548b489bca4bff70127c6cfbde259b1
Rawand Dargali <rdargali@yahoo.com>
2020-07-15 22:52:46 -0500

    deleting products

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,18 @@ const models = require("../models");
 
 let uniqueFilename = " ";
 
+router.post("/delete-product", async (req, res) => {
+  let productId = parseInt(req.body.productId);
+
+  let result = await models.Product.destroy({
+    where: {
+      id: productId,
+    },
+  });
+
+  res.redirect("/users/products");
+});
+
 router.get("/products", async (req, res) => {
   let products = await models.Product.findAll({
     where: {
