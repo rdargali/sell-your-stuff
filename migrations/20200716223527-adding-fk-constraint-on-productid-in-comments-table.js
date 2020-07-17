cc87e9091cd5d15dc278f6838f6d5e6f79b2f81d
Rawand Dargali <rdargali@yahoo.com>
2020-07-17 03:48:56 -0500

    small note

--- migrations/20200716223527-adding-fk-constraint-on-productid-in-comments-table.js
+++ migrations/20200716223527-adding-fk-constraint-on-productid-in-comments-table.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addConstraint("Comments", {
-      fields: ["productId"],
+      fields: ["productId"], //different from azam
       type: "FOREIGN KEY",
       name: "postid-fk-in-comments",
       references: {
